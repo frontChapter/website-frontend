b5d1192c5faead5b9c815389de042efdc750ae91
sys113 <051.sys113@gmail.com>
2024-01-14 18:00:05 +0330

    chore: add dana font to assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@/styles/globals.css';
-import Providers from './providers'
+import Providers from './providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,9 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
-        {children}
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
