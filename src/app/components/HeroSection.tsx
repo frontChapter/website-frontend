c325a800be3ea029518ee74a717f2f10e8ff681d
sys113 <051.sys113@gmail.com>
2024-01-15 16:46:25 +0330

    chore: add title, description and detail to hero section

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,7 +1,47 @@
 export default function HeroSection() {
   return (
     <section>
-      <p>سلام دنیا!</p>
+      <div className="container flex flex-col items-center space-y-4">
+        {/* title */}
+        <h1 className="w-full max-w-2xl text-3xl font-extrabold leading-[45px]">
+          دومین همایش <span className="text-orange-500">فرانت‌اند</span> ایران{' '}
+          <span className="text-green-500">اسفند ۱۴۰۲</span> در فریدون‌کنار،
+          مازندران
+        </h1>
+        {/* description */}
+        <h2 className="w-full max-w-2xl text-lg text-zinc-400">
+          همایش فرانت چپتر محلی برای گردهمایی و به اشتراک‌گذاری تجربیات
+          برنامه‌نویسان، متخصصین و علاقه‌مندان حوزه فرانت‌اند
+        </h2>
+        {/* detail */}
+        <div className="grid w-full max-w-2xl grid-cols-2">
+          {[
+            {
+              title: 'زمان',
+              description: '۱۰ اسفند ۱۴۰۲',
+            },
+            {
+              title: 'سخنرانان',
+              description: '۱۸ نفر',
+            },
+            {
+              title: 'شهر',
+              description: 'فریدون‌کنار، مازندران',
+            },
+            {
+              title: 'مکان',
+              description: 'مجتمع تفریحی رجایی',
+            },
+          ].map((item, index) => {
+            return (
+              <div key={index} className="">
+                <p className="text-sm text-zinc-50">{item.title}</p>
+                <p className="text-orange-500">{item.description}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </section>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="container py-4">
+    <main className="container py-8 space-y-6">
       <HeroSection />
     </main>
   );
