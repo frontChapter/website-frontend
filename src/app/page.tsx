3c73f89e3906b518d7e537ac490878fef88ddd9b
Amir Hossein Karimi <amir.hossein7464@gmail.com>
2024-01-19 02:07:02 +0330

    fix: remove container & padding from main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from '@/components/Navbar';
 
 export default function Home() {
   return (
-    <main className="container px-4 py-3 lg:px-20 lg:py-4">
+    <main>
       <Navbar />
     </main>
   );
