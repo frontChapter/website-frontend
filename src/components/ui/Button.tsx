c3d4f4166d0302faab88b77455850124961c6e7f
Amir Hossein Karimi <amir.hossein7464@gmail.com>
2024-01-19 05:53:20 +0330

    refactor: Button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -5,7 +5,13 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/utils/styles";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
+  cn(
+    "inline-flex items-center justify-center whitespace-nowrap rounded-md",
+    "text-sm font-medium ring-offset-white transition-colors lg:text-base",
+    "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2",
+    "disabled:pointer-events-none disabled:opacity-50",
+    "dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
+  ),
   {
     variants: {
       variant: {
@@ -19,9 +25,9 @@ const buttonVariants = cva(
         link: "text-slate-900 underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-3 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
+        default: "px-3 py-2",
+        sm: "rounded-md px-3",
+        lg: "rounded-md px-8",
         icon: "h-10 w-10",
       },
     },
