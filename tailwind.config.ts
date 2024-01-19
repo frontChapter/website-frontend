743cad6c2b4641c772fcdbc454a49f8ee07476b4
Amir Hossein Karimi <amir.hossein7464@gmail.com>
2024-01-19 05:15:55 +0330

    feat: increase tailwind default transitionDuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,6 +33,9 @@ const config = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      transitionDuration: {
+        DEFAULT: '250ms',
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
