fa50806e8df6656dc07bbd2c51c08d9083fa1ffc
Amir Hossein Karimi <amir.hossein7464@gmail.com>
2024-01-20 01:29:19 +0330

    refactor: extract HeroImages as stand-alone component

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -3,12 +3,7 @@
 import Image from "next/image";
 import imageVectorOrange from "@/assets/images/home/hero-section-vector-orange.svg";
 import imageVectorGreen from "@/assets/images/home/hero-section-vector-green.svg";
-import imageOne from "@/assets/images/home/hero-section-image-1.png";
-import imageTwo from "@/assets/images/home/hero-section-image-2.png";
-import imageThree from "@/assets/images/home/hero-section-image-3.png";
-import imageFour from "@/assets/images/home/hero-section-image-4.png";
-import imageFive from "@/assets/images/home/hero-section-image-5.png";
-import { cn } from "@/utils/styles";
+import HeroImages from "./HeroImages";
 
 export default function HeroSection() {
   return (
@@ -72,39 +67,7 @@ export default function HeroSection() {
           </div>
         </div>
       </div>
-
-      <div className="mt-32 flex snap-x scroll-px-4 grid-flow-col grid-cols-3 grid-rows-2 flex-nowrap gap-4 overflow-x-auto px-4 xl:container *:shrink-0 xl:grid xl:gap-x-8 xl:gap-y-14">
-        {[
-          {
-            className: "justify-end",
-            src: imageOne,
-          },
-          {
-            className: "",
-            src: imageTwo,
-          },
-          {
-            className: "row-span-2 items-center",
-            src: imageThree,
-          },
-          {
-            className: "justify-end",
-            src: imageFour,
-          },
-          {
-            className: "",
-            src: imageFive,
-          },
-        ].map(({ className, src }, index) => (
-          <div key={index} className={cn("flex snap-center", className)}>
-            <Image
-              src={src}
-              className={cn("w-40 md:w-60 lg:w-72 xl:w-[360px]")}
-              alt=""
-            />
-          </div>
-        ))}
-      </div>
+      <HeroImages />
     </section>
   );
 }
