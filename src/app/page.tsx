4a01959ef9f18759a784256d30d0ca6d6f0fd43c
Amirhossein Zareian <amirhosseinzareian01@gmail.com>
2024-01-21 19:29:11 +0330

    feat: use OtherParticipants component in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero/Hero";
 import Navbar from "@/components/Navbar";
+import OtherParticipants from "./components/OtherParticipants/OtherParticipants";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <OtherParticipants />
     </main>
   );
 }
