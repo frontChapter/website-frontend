d4725e2c74ad9d5e1943c543faf266c8c8a0b604
Amir Hossein Karimi <amir.hossein7464@gmail.com>
2024-01-22 20:55:55 +0330

    feat: update `<Speakers>` styles

--- src/app/components/Speakers/Speakers.tsx
+++ src/app/components/Speakers/Speakers.tsx
@@ -6,12 +6,13 @@ import SpeakerCard from "./SpeakerCard";
 export default function Speakers() {
   return (
     <section className="py-6 lg:py-12">
-      <div className="container space-y-16">
+      <div className="container flex flex-col items-stretch gap-2 lg:gap-16">
         {/* head */}
         <div className="flex flex-col items-center gap-2">
-          <RiMicLine className="size-12 fill-orange-500 lg:size-16" />
+          <RiMicLine className="size-12 text-orange-500 lg:size-16" />
           <h3 className="text-2xl font-bold lg:text-3xl">سخنرانان همایش</h3>
         </div>
+
         {/* body */}
         <div className="flex w-full flex-wrap justify-center gap-y-6 sm:gap-y-8 lg:gap-y-10">
           {speakersData.map((item, index) => {
