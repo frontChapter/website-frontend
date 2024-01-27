88b37b344139eee7160cdc68416b0b14d45651cb
Amir Hossein Karimi <amir.hossein7464@gmail.com>
2024-01-28 02:00:30 +0330

    feat: update `<Plan>` style

--- src/app/components/AdditionalPlans/Plan.tsx
+++ src/app/components/AdditionalPlans/Plan.tsx
@@ -4,7 +4,6 @@ import {
   RiCalendarLine,
   RiHotelLine,
   RiMapPinTimeLine,
-  RiStarFill,
   RiTimeLine,
 } from "react-icons/ri";
 
@@ -21,15 +20,12 @@ const Plan = ({
   transport,
 }: AdditionalPlan) => {
   return (
-    <div
-      className="mx-4 flex min-h-64
-   max-w-[850px] flex-col gap-y-5 rounded-xl border-2 border-zinc-700 bg-zinc-800 px-4 md:flex-row-reverse md:gap-4"
-    >
-      <div className="pt-4 md:border-r md:border-zinc-700 md:pb-4 md:pr-4">
+    <div className="mx-4 flex min-h-64 max-w-md flex-col gap-y-5 rounded-xl border-2 border-zinc-700 bg-zinc-800 md:max-w-[850px] md:flex-row-reverse md:gap-4">
+      <div className="flex shrink-0 items-center p-4 md:w-1/3 md:border-r md:border-zinc-700 md:p-6">
         <Image
           src={image.desktop.src}
           alt={image.desktop.alt}
-          className="hidden size-52 rounded-xl md:flex"
+          className="hidden w-full rounded-xl md:flex"
         />
         <Image
           src={image.mobile.src}
@@ -37,53 +33,43 @@ const Plan = ({
           className="w-full rounded-xl md:hidden "
         />
       </div>
-      <div className="flex flex-1 flex-col  gap-y-5 pb-4 md:items-start md:justify-between md:pt-4">
-        <div className="space-y-1">
-          <div className=" md:flex md:justify-between">
-            <p className=" text-2xl font-bold md:text-start md:text-3xl">
-              {title}
-            </p>
+      <div className="flex grow flex-col gap-y-5 p-4 md:items-start md:justify-between md:p-6">
+        <div className="flex flex-col gap-3">
+          <div className="md:flex md:justify-between">
+            <p className="text-2xl font-bold md:text-3xl">{title}</p>
           </div>
-          <p className=" text-lg leading-7 text-zinc-200 md:text-start md:text-xl">
+          <p className="text-lg leading-7 text-zinc-200 md:text-xl">
             {description}
           </p>
         </div>
         {place && (
-          <div className="w-full">
+          <div className="flex w-full flex-col gap-4">
             <div className="mt-2 flex justify-between">
-              <span className=" text-2xl font-semibold text-zinc-300 md:text-start md:text-3xl">
-                {place.name}
-              </span>
-              <span className="text-xl text-yellow-500 md:hidden">۵ ستاره</span>
-              <div className=" hidden md:flex">
-                {Array.from({ length: place.rate.value }).map((_, index) => (
-                  <RiStarFill
-                    key={index + place.rate.value}
-                    className="text-lg text-yellow-500"
-                  />
-                ))}
+              <div className="flex items-center gap-2 text-2xl font-semibold text-zinc-300 md:text-start">
+                <RiHotelLine />
+                <span>{place.name}</span>
               </div>
             </div>
-            <div className="md:flex md:justify-between">
-              <div className="mt-3 flex items-center gap-2">
-                <RiTimeLine className="text-xl" />
-                <span className="text-zinc-200">{time}</span>
-              </div>
-              <div className=" flex items-center gap-2">
-                <RiCalendarLine className="text-xl" />
+            <div className="flex flex-col gap-3 text-lg md:flex-row md:justify-between lg:items-center">
+              <div className="flex items-center gap-2">
+                <RiCalendarLine />
                 <span className="text-zinc-200">{date}</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <RiTimeLine />
+                <span className="text-zinc-200">{time}</span>
               </div>
             </div>
           </div>
         )}
         {transport && (
-          <div className="w-full">
-            <div className="mt-3 flex flex-nowrap items-center text-zinc-200">
-              <RiMapPinTimeLine className="text-2xl" />
-              <hr className="border-1 mx-5 flex-1 border-zinc-700" />
-              <RiBusLine className="text-2xl" />
-              <hr className="border-1 mx-5 flex-1 border-zinc-700" />
-              <RiHotelLine className="text-2xl" />
+          <div className="mt-auto w-full">
+            <div className="flex flex-nowrap items-center gap-5 text-2xl text-zinc-200 lg:text-3xl">
+              <RiMapPinTimeLine />
+              <hr className="grow border-zinc-700" />
+              <RiBusLine />
+              <hr className="grow border-zinc-700" />
+              <RiHotelLine />
             </div>
             <div className="mt-3 flex justify-between ">
               <p className="text-lg text-zinc-200">
