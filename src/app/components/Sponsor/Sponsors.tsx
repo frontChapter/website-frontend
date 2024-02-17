f07e4469b2be48297b62a5b7d2fb2bcf3dcfe2d9
Saleh <ordinarysaleh@pm.me>
2024-02-17 18:08:09 +0330

    chore: add links and change styles

--- src/app/components/Sponsor/Sponsors.tsx
+++ src/app/components/Sponsor/Sponsors.tsx
@@ -1,8 +1,8 @@
 import { buttonVariants } from "@/components/ui/Button";
 import { sponsors } from "@/data/sponsersData";
-import { FaHandHoldingHeart } from "react-icons/fa";
 import Image from "next/image";
 import Link from "next/link";
+import { FaHandHoldingHeart } from "react-icons/fa";
 
 const Sponsors = () => {
   return (
@@ -11,25 +11,36 @@ const Sponsors = () => {
         <h3 className="mt-16 text-center text-2xl font-bold text-white md:text-4xl">
           حامی‌های مالی و معنوی فرانت چپتر
         </h3>
-        <div className="w-full border-b-2 border-t-2 border-zinc-700 bg-zinc-800">
-          <div className="flex items-center justify-center gap-6 px-6 py-7 md:gap-12 md:px-12 md:py-14">
+
+        <div className="w-full">
+          <div className="flex items-center justify-center gap-6 px-6 py-1 md:gap-12 md:px-12 md:py-2">
             {sponsors.map((sponsor) => (
-              <Image
-                src={sponsor.src}
-                alt={sponsor.alt}
-                className={`${sponsor.styles}`}
+              <div
                 key={sponsor.title}
-              />
+                className="transition-all hover:scale-110"
+              >
+                <Link
+                  href={sponsor.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Image
+                    src={sponsor.src}
+                    alt={sponsor.alt}
+                    className={sponsor.styles}
+                  />
+                </Link>
+              </div>
             ))}
           </div>
         </div>
-        <p className="text-center text-sm font-normal leading-6 text-zinc-200 md:text-base">
+        <p className="max-w-96 text-center text-sm font-normal leading-6 text-zinc-200 md:text-base">
           فرانت چپتر برای ادامه و گسترش فعالیت های خود در جهت افزایش آگاهی جامعه
           فرانت اند و همچنین برگزاری چنین رویداد هایی نیاز به حمایت های شما
           دارد.
         </p>
         <Link
-          href="#?"
+          href="mailto:frontchapter.ir@gmail.com"
           target="_blank"
           className={`${buttonVariants({
             variant: "primary",
